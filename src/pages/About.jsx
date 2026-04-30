a45a5180898469a2765582e9a9f9d7698f8eaf1a
nonynony28-netizen <nonynony28@gmail.com>
2026-04-30 03:18:09 +0200

    Add About page with company information in Arabic

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <main className="page">
+      <h1>من نحن</h1>
+      <p>الإنارة الحديثة شركة متخصصة في حلول الإضاءة والتأسيس الكهربائي.</p>
+    </main>
+  );
+}
+
+export default About;
