e4648b11fdabdb9c17cefe5f130e5cd23d7ee797
nonynony28-netizen <nonynony28@gmail.com>
2026-04-30 03:17:10 +0200

    Add Partners component to display partner list

---
+++ src/pages/Partners.jsx
@@ -0,0 +1,19 @@
+const partners = ["شركة 1", "شركة 2", "شركة 3"];
+
+function Partners() {
+  return (
+    <main className="page">
+      <h1>الشركاء</h1>
+
+      <div className="grid">
+        {partners.map((p) => (
+          <div className="card" key={p}>
+            <h3>{p}</h3>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default Partners;
