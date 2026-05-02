9665420283e7e2faa8550a98c82fff3f1c51d33c
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 18:33:59 +0200

    Refactor main.jsx to use ReactDOM for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Brands from "./pages/Brands";
-import Partners from "./pages/Partners";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Branches from "./pages/Branches";
-import Contact from "./pages/Contact";
+const rootElement = document.getElementById("root");
 
-function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/brands" element={<Brands />} />
-        <Route path="/partners" element={<Partners />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/branches" element={<Branches />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </HashRouter>
-  );
+if (!rootElement) {
+  throw new Error("Root element not found");
 }
 
-export default App;
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
