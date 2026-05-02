e5459c987705e51defcb4fea0636e5fa2de5ea06
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 02:44:08 +0200

    Fix import path for Index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./Index.css";
 
 // 💡 رسالة تتبع للمبرمج للتأكد من عمل الكود
 console.log("🚀 جاري تشغيل تطبيق React...");
