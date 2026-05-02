059e969f30cea70f8e44f186452ddc48a3e3d42d
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 18:31:41 +0200

    Refactor App component to use React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Brands from "./pages/Brands";
+import Partners from "./pages/Partners";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Branches from "./pages/Branches";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div style={{color:"#fff",background:"#111",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:32}}>TEST OK</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/partners" element={<Partners />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/branches" element={<Branches />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(<App />);
+export default App;
