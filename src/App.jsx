1e86c61b3ee5859d2d88c8843948b8aeb2c94abe
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 18:01:00 +0200

    Refactor App component to remove routing
    Removed routing components and added a test message.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Brands from "./pages/Brands";
-import Partners from "./pages/Partners";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Branches from "./pages/Branches";
-import Contact from "./pages/Contact";
-
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/brands" element={<Brands />} />
-        <Route path="/partners" element={<Partners />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/branches" element={<Branches />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </HashRouter>
+    <div style={{
+      minHeight: "100vh",
+      background: "#111",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "40px"
+    }}>
+      TEST OK
+    </div>
   );
 }
 
