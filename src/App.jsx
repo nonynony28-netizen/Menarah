58bc762748ee9c12e226305509dd57f85827d432
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 20:22:51 +0200

    Add Navbar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Brands from "./pages/Brands";
@@ -13,6 +15,8 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <HashRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
