f952d92098d4ce6386cfb93e5e5973390d97a0d2
nonynony28-netizen <nonynony28@gmail.com>
2026-05-02 17:52:19 +0200

    Change index.css import to Index.css
    Updated import statement to use 'Index.css' instead of 'index.css'.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // مهم: بحرف i صغير
+import "./Index.css"; // مهم: بحرف i صغير
 
 console.log("🚀 تشغيل تطبيق React...");
 
