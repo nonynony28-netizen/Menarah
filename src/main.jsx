dd41051650fe8427cbad2295f23158fad31961f1
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 17:28:33 +0200

    Change import path for Splash component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Splash from "./pages/Splash.jsx";
+import Splash from "./Splash";
 import "./index.css";
 
 function Root() {
