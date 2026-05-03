eb25e44bdf8b65c1f2a7aa0284b9f64bf73890f7
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 20:17:10 +0200

    Enable Navbar and set up routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-// ❌ عطّل Navbar
-{/* <Navbar /> */}
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Brands from "./pages/Brands";
+import Partners from "./pages/Partners";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Branches from "./pages/Branches";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <div>مرحبا</div>;
+  return (
+    <HashRouter>
+      
+      {/* Navbar ثابت */}
+      <Navbar />
+
+      {/* محتوى الصفحات */}
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/branches" element={<Branches />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+
+    </HashRouter>
+  );
 }
 
 export default App;
