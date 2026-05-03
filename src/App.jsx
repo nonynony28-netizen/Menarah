11c23f2f16c9cbc880f6a703ef6cc31d135baa1a
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 22:39:48 +0200

    Refactor App component to remove routing
    Removed routing and navbar components, added a test div.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-
 function App() {
   return (
-    <HashRouter>
-
-      {/* Navbar */}
-      <Navbar />
-
-      {/* الصفحات */}
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-
-    </HashRouter>
+    <div style={{
+      color: "white",
+      background: "red",
+      padding: "50px",
+      fontSize: "30px"
+    }}>
+      TEST VISIBLE
+    </div>
   );
 }
 
