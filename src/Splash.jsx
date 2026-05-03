98cda9675c9bcb8f33837ac8844097889e27eb9a
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 04:03:34 +0200

    Refactor Splash component functionality and styling
    Updated Splash component to ensure onFinish is called only if defined and added class to image.

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
-import splash from "../picture/hero.jpg";
+import splash from "../picture/hero.jpg"; // تأكد من المسار
 
 function Splash({ onFinish }) {
-
   useEffect(() => {
     const timer = setTimeout(() => {
-      onFinish();
-    }, 3000);
+      if (onFinish) onFinish(); // ينتقل إلى App.jsx
+    }, 3000); // 3 ثواني
 
     return () => clearTimeout(timer);
   }, [onFinish]);
 
   return (
     <div className="splash-screen">
-      <img src={splash} alt="welcome" />
+      <img src={splash} alt="welcome" className="splash-img" />
     </div>
   );
 }
