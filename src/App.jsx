11a1cd08832959a0c11d6dfd1eeb39f0a1362b98
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 20:05:48 +0200

    Refactor App component for testing
    Removed unused imports and commented-out code, replaced return statement with a test heading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-// ❗ عطّل Navbar مؤقتاً
-// import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-
-// ❗ عطّل باقي الصفحات مؤقتاً
-// import Products from "./pages/Products";
-// import Brands from "./pages/Brands";
-// import Partners from "./pages/Partners";
-// import Projects from "./pages/Projects";
-// import About from "./pages/About";
-// import Branches from "./pages/Branches";
-// import Contact from "./pages/Contact";
-
 function App() {
-  return (
-    <HashRouter>
-
-      {/* Navbar متوقف مؤقتاً */}
-      {/* <Navbar /> */}
-
-      <div className="app-container">
-        <Routes>
-
-          {/* فقط الصفحة الرئيسية */}
-          <Route path="/" element={<Home />} />
-
-        </Routes>
-      </div>
-
-    </HashRouter>
-  );
+  return <h1 style={{color:"white"}}>Test</h1>;
 }
 
 export default App;
