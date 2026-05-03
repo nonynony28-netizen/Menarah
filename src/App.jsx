c4b4808307a1f3c1945885309b3977b25d4202b6
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 22:35:55 +0200

    Implement routing with HashRouter and Routes
    Refactor App component to use HashRouter and Routes for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <h1 style={{color:"red", fontSize:"40px"}}>
-      NEW PROJECT WORKING
-    </h1>
+    <HashRouter>
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* الصفحات */}
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
+    </HashRouter>
   );
 }
 
