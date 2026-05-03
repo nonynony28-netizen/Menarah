e1f8090070d33f7f2a81d763adcdfc3bb2c4deb9
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 21:52:06 +0200

    Implement routing with HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div style={{color:"white", padding:"40px"}}>
-      APP WORKING
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
