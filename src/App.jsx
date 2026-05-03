e5ac92cd47f4cd72b5dc557c0bb4368db810cf74
nonynony28-netizen <nonynony28@gmail.com>
2026-05-03 22:46:33 +0200

    Remove App function and keep header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-function App() {
-  return (
-    <div style={{
-      color: "white",
-      background: "red",
-      padding: "50px",
-      fontSize: "30px"
-    }}>
-      <h1>FINAL TEST 123</h1>
-    </div>
-  );
-}
-
-export default App;
+<h1>FINAL TEST 123</h1>
